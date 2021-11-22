f200e1514de6c8377e30bae588fc5004ac24a974
AngeloBastos <angelohbastos@gmail.com>
2021-11-21 23:45:37 -0300

    add app to repository

---
+++ app/src/main/java/com/example/tasktodo/Model/ToDoModel.java
@@ -0,0 +1,19 @@
+package com.example.tasktodo.Model;
+
+public class ToDoModel extends TaskId {
+    private String task;
+    private  String due;
+    private int status;
+
+    public String getTask() {
+        return task;
+    }
+
+    public String getDue() {
+        return due;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
